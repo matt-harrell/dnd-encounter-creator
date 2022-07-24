9f598f95cbe3ec0fcf7fd4fde342db39d20657e7
Matt Harrell <matthew.629@hotmail.com>
2022-07-24 19:02:42 -0400

    add type fix

--- src/components/MonsterCard/MonsterCard.tsx
+++ src/components/MonsterCard/MonsterCard.tsx
@@ -7,6 +7,7 @@ import {
   failedToLoadMonster,
   
 } from "../../features/MonsterCardSlice";
+import { AppDispatch } from "../../app/store";
 
 
 // fetch('https://www.dnd5eapi.co/api/monsters/adult-black-dragon')
@@ -14,7 +15,7 @@ import {
 //   .then(data => console.log(data.name));
 
 const MonsterCard = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   const monsterCardContent = useSelector(selectMonsterCard);
 
   useEffect(() =>{
