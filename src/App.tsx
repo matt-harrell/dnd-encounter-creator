70a23d21c19ed975a6dc67d8cee1a050aa416586
Matt Harrell <matthew.629@hotmail.com>
2022-07-26 22:48:27 -0400

    add start of ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import MonsterCard from './components/MonsterCard/MonsterCard';
 import SearchBar from './components/MonsterCard/SearchBar/searchBar';
 import './App.css';
+import { Grid, } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <SearchBar/>
-      <MonsterCard/>
+      <Grid container spacing={2} sx={{paddingY:5}}>
+        <Grid item xs={12}>
+          <SearchBar />
+        </Grid>
+        <Grid item xs={12}>
+          <MonsterCard />
+        </Grid>
+      </Grid>
     </div>
   );
 }
