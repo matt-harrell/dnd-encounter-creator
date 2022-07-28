6282a3ebb723926506875f4586ae76ad8733f8d9
Matt Harrell <matthew.629@hotmail.com>
2022-07-27 22:33:58 -0400

    add more card layout

--- src/components/MonsterCard/MonsterCard.tsx
+++ src/components/MonsterCard/MonsterCard.tsx
@@ -13,15 +13,25 @@ import { Grid,Typography,Box } from "@mui/material";
 const MonsterCard = () => {
   const dispatch = useDispatch<AppDispatch>();
   const monsterCardContent = useSelector(selectMonsterCard);
-
+  console.log(monsterCardContent);
+  
+  
   
 
 
     return (
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6} sx={{textAlign:'left'}}>
-            <Typography>Monster Name</Typography>
-            <Typography>Monster alinment</Typography>
+            <Typography component={'h2'} variant={'h3'}>Monster Name</Typography>
+            <Typography component={'p'} variant={'body1'}>{`size`} {`type`} {`alighnment`}</Typography>
+            <hr />
+            <Grid container spacing={1}>
+              {/* need to make a function to calc bonuses in monstercardslice some sort of conditional will work */}
+              <Grid item xs={2}>
+                <Typography>STR</Typography>
+                <Typography>{`statMods[1]`}</Typography>
+              </Grid>
+            </Grid>
           </Grid>
           <Grid item xs={12} sm={6} sx={{textAlign:'left'}}>
             <Typography>Monster Name</Typography>
