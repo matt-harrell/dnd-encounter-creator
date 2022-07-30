e6ea3e64ba139cfaf3a8fde221947a650c59db8f
Matt Harrell <matthew.629@hotmail.com>
2022-07-30 10:50:19 -0400

    add monster speed logic and build out statmod bloc

--- src/components/MonsterCard/MonsterCard.tsx
+++ src/components/MonsterCard/MonsterCard.tsx
@@ -13,7 +13,22 @@ import { Grid,Typography,Box } from "@mui/material";
 const MonsterCard = () => {
   const dispatch = useDispatch<AppDispatch>();
   const monsterCardContent = useSelector(selectMonsterCard);
-  console.log(monsterCardContent);
+
+  // condioanl logic to render speed for testing needs to be deleted after done
+  if (monsterCardContent.speed === undefined) {
+  } else {
+    let speedType:string[] = [];
+    for (const moveType in monsterCardContent.speed)
+    if (moveType === 'walk'){
+      speedType.push(monsterCardContent.speed[moveType])
+    } else{
+      speedType.push(`${moveType} ${monsterCardContent.speed[moveType]}`)
+    }
+    console.log(speedType)
+    // map array to add commas after each except to last element 
+  }
+
+  
   
   
   
@@ -25,12 +40,36 @@ const MonsterCard = () => {
             <Typography component={'h2'} variant={'h3'}>Monster Name</Typography>
             <Typography component={'p'} variant={'body1'}>{`size`} {`type`} {`alighnment`}</Typography>
             <hr />
+            <Box>
+            <Typography component={'p'} variant={'body1'}>Armor Class{`armor_class`}</Typography>
+            <Typography component={'p'} variant={'body1'}>Hit Points {`hit_points`} ({`hit_dice`})</Typography>
+            </Box>
+            <hr />
             <Grid container spacing={1}>
-              {/* need to make a function to calc bonuses in monstercardslice some sort of conditional will work */}
               <Grid item xs={2}>
                 <Typography>STR</Typography>
-                <Typography>{`statMods[1]`}</Typography>
+                <Typography>statMods[1]</Typography>
               </Grid>
+              <Grid item xs={2}>
+                <Typography>DEX</Typography>
+                <Typography>statMods[2]</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <Typography>CON</Typography>
+                <Typography>statMods[3]</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <Typography>INT</Typography>
+                <Typography>statMods[4]</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <Typography>WIS</Typography>
+                <Typography>statMods[5]</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <Typography>CHA</Typography>
+                <Typography>statMods[6]</Typography>
+              </Grid>              
             </Grid>
           </Grid>
           <Grid item xs={12} sm={6} sx={{textAlign:'left'}}>
