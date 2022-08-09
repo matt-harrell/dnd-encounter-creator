3d51db23870aae2fc13ecf70d4979347878fb9b8
Matt Harrell <matthew.629@hotmail.com>
2022-08-09 13:04:45 -0400

    add ecounter xp display

--- src/components/MonsterCard/encounterTable.tsx
+++ src/components/MonsterCard/encounterTable.tsx
@@ -2,13 +2,14 @@ import { Grid, Button } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 import { useSelector, useDispatch } from "react-redux";
 
-import { selectListOfMonsters, removeMonster, calcEncoutnerXP } from "../../features/encounterSlice";
+import { selectListOfMonsters, removeMonster, calcEncoutnerXP, selectEncounterExp } from "../../features/encounterSlice";
 import { setMonsterCardContent, setShowMonsterCard, showMonsterCard } from "../../features/MonsterCardSlice";
 
 
 const EcounterTable = () => {
     const monsterList = useSelector(selectListOfMonsters);
     const showMonster = useSelector(showMonsterCard);
+    const encounterExp = useSelector(selectEncounterExp);
     // console.log(monsterList);
     const dispatch = useDispatch();
     const handleViewClick = (index:number) => (e:any) =>{
@@ -28,11 +29,14 @@ const EcounterTable = () => {
     <>
     {monsterList.length !==0 && (
         <>
+        <Grid container spacing={2} sx={{paddingBottom:5}}>
+            <Grid item xs={12} sm={12}>Total Encoutner XP:{encounterExp}</Grid>
+        </Grid>
             {monsterList.map((monster,index) => (
-                <Grid container spacing={2} key={index} sx={{paddingY:.5}} >
+                <Grid container spacing={2} key={index} sx={{paddingY:.5,alignItems:'center'}} >
                     <Grid item xs={12} sm={4}>{monster.name}</Grid>
-                    <Grid item xs={6} sm={3}>{monster.xp}</Grid>
-                    <Grid item xs={6} sm={1}>{monster.challenge_rating}</Grid>
+                    <Grid item xs={6} sm={3}>XP: {monster.xp}</Grid>
+                    <Grid item xs={6} sm={1}>CR: {monster.challenge_rating}</Grid>
                     <Grid item xs={6} sm={3}>
                         <Button variant="contained" disableElevation onClick={handleViewClick(index)}>
                             View Monster
