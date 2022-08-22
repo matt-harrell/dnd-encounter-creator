8143f6e579ffcfb3b584303060eb5fe567a211e8
Matt Harrell <matthew.629@hotmail.com>
2022-08-21 22:04:40 -0400

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { createTheme, ThemeProvider, Box } from '@mui/material';
 import './App.css';
 import SearchBarDrawerCont from './components/SearchBarsDrawer/SearchBarDrawerCont';
 
+const theme = createTheme({
+  palette:{
+    secondary:{
+      main:'#D2B48C',
+      light:'#ebd3b4'
+    }
+  }
+})
 
 function App() {
   return (
-    <SearchBarDrawerCont/>
-    
-    
+    <ThemeProvider theme={theme}>
+      <Box sx={{bgcolor:'secondary.main',height:'100%'}}>
+        <SearchBarDrawerCont/>
+      </Box>
+    </ThemeProvider>
   );
 }
 
