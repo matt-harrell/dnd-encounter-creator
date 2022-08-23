5076aa20f77e35a1a78110e6d6cb9c7531697ca9
Matt Harrell <matthew.629@hotmail.com>
2022-08-23 13:31:22 -0400

    add danger color

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import { createTheme, ThemeProvider, Box } from '@mui/material';
 import './App.css';
 import SearchBarDrawerCont from './components/SearchBarsDrawer/SearchBarDrawerCont';
 
+declare module '@mui/material/styles' {
+
+  interface Palette {
+    danger: Palette['primary'];
+  }
+  interface PaletteOptions {
+    danger: PaletteOptions['primary'];
+  }
+}
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    danger: true;
+  }
+}
+
+
 const theme = createTheme({
   palette:{
     secondary:{
       main:'#D2B48C',
       light:'#ebd3b4'
+    },
+    danger:{
+      main:'#660000',
+      dark:'#390101',
+      contrastText: '#fff',
     }
   }
 })
